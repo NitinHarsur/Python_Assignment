4ac78d1da75db6e79f46e164833afa38134cf761
Nitin Harsur <122150786+NitinHarsur@users.noreply.github.com>
2023-11-23 15:30:53 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter  First Number:"))
+num2=int(input("Enter  Second Number:"))
+
+print("*****Welcome To calculator*****")
+
+print("Addition of Two Number is:",num1+num2)
+print("Subtraction of Two Number is:",num1-num2)
+print("Multiplication of Two Number is:",num1*num2)
+print("Division of Two Number is:",num1/num2)
